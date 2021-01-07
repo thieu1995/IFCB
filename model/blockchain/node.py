2a00fdcdac548fa75aaea130ee9671bff07bf850
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-01-07 21:29:33 +0700

    Add blockchain nodes

---
+++ model/blockchain/node.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu" at 19:38, 07/01/2021                                                               %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Nguyen_Thieu2                                  %
+#       Github:     https://github.com/thieu1995                                                        %
+# ------------------------------------------------------------------------------------------------------%
+
+from numpy import array
+from model.base import Base
+
+
+class Node(Base):
+
+    def __init__(self, id: int, name: str, location: array):
+        super().__init__(id, name, location)
+
+        self.alpha_sm = 0       # power consumption - data forwarding - standby mode
+        self.gamma_sm = 0       # power consumption - storage - standby mode
+
+        self.sigma_sm = 0       # cost - data forwarding - standby mode
+        self.omega_sm = 0       # cost - storage - standby mode
+
