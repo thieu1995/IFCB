098642042151a9b78a8635fd316c7ec49c84b404
Thieu Nguyen <nguyenthieu2102@gmail.com>
2021-01-21 10:38:47 +0700

    Keeping the first NSGA3 to later check

--- optimizer/root3.py
+++ optimizer/root3.py
@@ -7,28 +7,67 @@
 #       Github:     https://github.com/thieu1995                                                        %
 # ------------------------------------------------------------------------------------------------------%
 
-from time import time
-from config import Config
-from sys import exit
 from optimizer.root import Root
-from numpy import array, inf, zeros, argmin
+from numpy import array, inf
 from numpy.random import uniform
-from utils.schedule_util import matrix_to_schedule
-from uuid import uuid4
+from time import time
 from copy import deepcopy
 from math import sqrt
 from random import randint
+from utils.schedule_util import matrix_to_schedule
+from uuid import uuid4
+from config import Config
 
 
 class Root3(Root):
     ID_IDX = 0
     ID_POS = 1
     ID_FIT = 2
-    
 
     def __init__(self, problem=None, pop_size=10, epoch=2, func_eval=100000, lb=None, ub=None):
         super().__init__(problem, pop_size, epoch, func_eval, lb, ub)
         self.num_obj = 3
+
+    def fast_non_dominated_sort(self, pop: dict):
+        obj = [[] for _ in range(0, self.num_obj)]
+        for idx, item in pop.items():
+            for i in range(self.num_obj):
+                obj[i].append(item[self.ID_FIT][i])
+        size = len(obj[0])
+        front = []
+        num_assigned_individuals = 0
+        indv_ranks = [0 for _ in range(0, size)]
+        rank = 1
+
+        while num_assigned_individuals < size:
+            cur_front = []
+            for i in range(size):
+                if indv_ranks[i] > 0:
+                    continue
+                be_dominated = False
+
+                j = 0
+                while j < len(cur_front):
+                    idx_1 = cur_front[j]
+                    idx_2 = i
+                    if self.dominate(idx_1, idx_2, obj):
+                        be_dominated = True
+                        break
+                    elif self.dominate(idx_2, idx_1, obj):
+                        cur_front[j] = cur_front[-1]
+                        cur_front.pop()
+                        j -= 1
+                    j += 1
+
+                if (not be_dominated):
+                    cur_front.append(i)
+
+            for i in range(len(cur_front)):
+                indv_ranks[cur_front[i]] = rank
+            front.append(cur_front)
+            num_assigned_individuals += len(cur_front)
+            rank += 1
+        return front
 
     def create_solution(self):
         while True:
@@ -39,9 +78,9 @@ class Root3(Root):
                 idx = uuid4().hex
                 break
         return [idx, matrix, fitness]
-    
+
     def compute_ideal_points(self, pop, fronts, num_obj):
-        ideal_point = [0] * num_obj;
+        ideal_point = [0] * num_obj
         conv_pop = deepcopy(pop)
         for i in range(num_obj):
             
@@ -58,7 +97,8 @@ class Root3(Root):
                 for k in range(len(fronts[j])):
                     idx = list(conv_pop.keys())[fronts[j][k]]
                     conv_pop[idx][self.ID_FIT][i] -= minf
-        
+
+        print(ideal_point)
         return ideal_point, conv_pop
 
     def ASF(self, objs, weight):
@@ -102,7 +142,9 @@ class Root3(Root):
                 if (duplicate):
                     break
                 duplicate = (extreme_points[i] == extreme_points[j])
-                
+        ## Loi vong lap for ben tren, luon tra gia tri True
+
+
         if duplicate:
             for i in range(len(intercepts)):
                 idx = list(pop.keys())[extreme_points[i]]
@@ -206,13 +248,13 @@ class Root3(Root):
             if num_mem[r] == min_size:
                 min_rps.append(r)
         if len(min_rps) == 0:
-            return -1;
+            return -1
         
         return min_rps[randint(0, len(min_rps) - 1)]
     
     def FindClosestMember(self, potentials):
         min_dist = inf
-        min_indv = -1;
+        min_indv = -1
         for i in range(len(potentials)):
             if potentials[i][1] < min_dist:
                 min_dist = potentials[i][1]
@@ -226,13 +268,13 @@ class Root3(Root):
         return potentials[randint(0, len(potentials) - 1)][0]
     
     def SelectClusterMember(self, rp, n_mems):
-        chosen = -1;
+        chosen = -1
         if (len(rp) > 0):
             if n_mems == 0: # currently has no member:
                 chosen =  self.FindClosestMember(rp)
             else:
                 chosen =  self.RandomMember(rp)
-        return chosen;
+        return chosen
     
     def dominate(self, id1, id2, obj):
         better = False
@@ -242,52 +284,6 @@ class Root3(Root):
             elif obj[i][id1] < obj[i][id2]:
                 better = True
         return better
-    
-    
-    # Function to carry out NSGA-II's fast non dominated sort
-    def fast_non_dominated_sort(self, pop: dict):
-        obj = [[] for _ in range(0, self.num_obj)]
-        for idx, item in pop.items():
-            for i in range(self.num_obj):
-                obj[i].append(item[self.ID_FIT][i])
-        size = len(obj[0])
-        front = []
-        num_assigned_individuals = 0
-        indv_ranks = [0 for _ in range(0, size)]
-        rank = 1
-        
-        while num_assigned_individuals < size:
-            cur_front = []
-            for i in range(size):
-                if indv_ranks[i] > 0:
-                    continue
-                be_dominated = False
-                
-                j = 0
-                while j < len(cur_front):
-                    idx_1 = cur_front[j]
-                    idx_2 = i
-                    if self.dominate(idx_1, idx_2, obj):
-                        be_dominated = True
-                        break
-                    elif self.dominate(idx_2, idx_1, obj):
-                        cur_front[j] = cur_front[-1]
-                        cur_front.pop()
-                        j -= 1
-                    j += 1
-                        
-                if(not be_dominated):
-                    cur_front.append(i)
-                    
-            for i in range(len(cur_front)):
-                indv_ranks[ cur_front[i] ] = rank
-            front.append(cur_front)
-            num_assigned_individuals += len(cur_front)
-            rank += 1
-        return front
-
-    def evolve(self, pop=None, fe_mode=None, epoch=None, g_best=None):
-        pass
 
     def train(self):
         if Config.METRICS == "pareto" and self.__class__.__name__ not in Config.MULTI_OBJECTIVE_SUPPORTERS:
@@ -329,4 +325,3 @@ class Root3(Root):
                 solutions[idx] = pop[idx]
                 g_best.append(pop[idx][self.ID_FIT])
             return solutions, array(g_best), g_best_dict
-
